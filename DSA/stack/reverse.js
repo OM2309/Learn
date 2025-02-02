9bf6abdbdd0e0bb309edfd15d90596582a3139d0
Om <23anuragsharma@gmail.com>
2025-02-02 23:15:11 +0530

    dd

---
+++ DSA/stack/reverse.js
@@ -0,0 +1,25 @@
+// Reverse a String using Stack
+
+class ReverseString {
+  constructor() {
+    this.stack = [];
+  }
+
+  reverse(str) {
+    for (let char of str) {
+      this.stack.push(char);
+    }
+
+    let reversedStr = "";
+    while (this.stack.length > 0) {
+      reversedStr += this.stack.pop();
+    }
+
+    return reversedStr;
+  }
+}
+
+const reverseStringStack = new ReverseString();
+
+console.log(reverseStringStack.reverse("hello"));
+console.log(reverseStringStack.reverse("world"));
