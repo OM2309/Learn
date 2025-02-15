21b3338e337a2ab0cd7e95f4e8fecb7fdbf4b83e
Anurag <129600042+OM2309@users.noreply.github.com>
2025-02-15 22:46:49 +0530

    Update

--- DSA/array/ques.js
+++ DSA/array/ques.js
@@ -1,7 +1,4 @@
-// Problem: Given an array of integers nums and an integer target, return the indices of the two numbers that add up to target.
-// Input: nums = [2, 7, 11, 15], target = 9
-// Output: [0, 1]
-// Explanation: nums[0] + nums[1] = 2 + 7 = 9
+
 
 
 function twoSum(nums, target) {
