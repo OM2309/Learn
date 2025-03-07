ec44e0e5e0ac56a26ecce949973d88a8996ccb50
Anurag <129600042+OM2309@users.noreply.github.com>
2025-03-07 23:52:13 +0530

    Update

--- DSA/array/966.js
+++ DSA/array/966.js
@@ -1,6 +1,5 @@
 function spellChecker(wordlist, queries) {
-
-   let exactWords = new Set(wordlist);
+ let exactWords = new Set(wordlist);
 
   // ✅ Step 2: Capitalization match aur vowel errors ke liye maps banao
   let capMap = new Map();
