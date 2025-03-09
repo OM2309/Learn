7411f0af76b28b518e55e27c90e5883086c43e6e
Om <23anuragsharma@gmail.com>
2025-03-09 13:03:49 +0530

    learn basic in dsa

---
+++ DSA/array/max.js
@@ -0,0 +1,40 @@
+// const arr = [1, 2, 4, 3, 6, 76, 2, 3, 11];
+// let max1 = -Infinity;
+// let max2 = -Infinity;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max1) {
+//     max2 = max1;
+//     max1 = arr[i];
+//   }
+// }
+
+// console.log(max);
+
+// let array = [1, 2, 3, 4, 5, 6];
+
+// for (let i = array.length - 1; i >= 0; i--) {
+//   console.log(array[i]);
+// }
+
+// let arr = [1, 0, 1, 0, 1, 1, 0, 1];
+
+// let start = 0;
+// let end = arr.length - 1;
+
+// while (start < end) {
+//   if (arr[start] === 1 && arr[end] === 0) {
+//     let temp = arr[start];
+//     arr[start] = arr[end];
+//     arr[end] = temp;
+
+//     start++;
+//     end--;
+//   } else if (arr[start] === 0) {
+//     start++;
+//   } else if (arr[end] === 1) {
+//     end--;
+//   }
+// }
+
+// console.log(arr);
--- DSA/leetcode/occurences.js
+++ DSA/leetcode/occurences.js
@@ -1,16 +1,41 @@
-const arr = [1, 2, 2, 2, 3, 4, 5];
+// const arr = [1, 2, 2, 2, 3, 4, 5];
 
-function occurences(arr, key) {
-  let obj = {};
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === key) {
-      ++count;
-      obj[arr[i]] = count;
+// function occurences(arr, key) {
+//   let obj = {};
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === key) {
+//       ++count;
+//       obj[arr[i]] = count;
+//     }
+//   }
+
+//   return obj;
+// }
+
+// console.log(occurences(arr, 2));
+
+// Input: arr = [1, 2, 2, 2, 3, 4, 5], key = 2
+// Output: 1  (because the first occurrence of 2 is at index 1)
+
+function binarySearchOccurences(arr, key) {
+  let start = 0;
+  let end = arr.length - 1;
+  let result = -1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === key) {
+      result = mid;
+      end = mid - 1;
+    } else if (arr[mid] > key) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
     }
   }
 
-  return obj;
+  return result;
 }
 
-console.log(occurences(arr, 2));
+console.log(binarySearchOccurences([1, 2, 2, 2, 2, 4, 5, 6], 2));
