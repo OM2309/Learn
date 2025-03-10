ff3c941529f2b80d7165c191d4a29f0036401784
Om <23anuragsharma@gmail.com>
2025-03-10 15:49:30 +0530

    practice

--- DSA/interview/question.js
+++ DSA/interview/question.js
@@ -44,40 +44,66 @@
 
 // console.log(reverseArray(arr));
 
-function secondLargest(arr) {
-  let firstLargestElement = -Infinity;
-  let secondLargestElement = -Infinity;
-  let firstSmallestElement = Infinity;
-  let secondSmallestElement = Infinity;
+// function secondLargest(arr) {
+//   let firstLargestElement = -Infinity;
+//   let secondLargestElement = -Infinity;
+//   let firstSmallestElement = Infinity;
+//   let secondSmallestElement = Infinity;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > firstLargestElement) {
-      secondLargestElement = firstLargestElement;
-      firstLargestElement = arr[i];
-    } else if (
-      arr[i] > secondLargestElement &&
-      arr[i] !== firstLargestElement
-    ) {
-      secondLargestElement = arr[i];
-    }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > firstLargestElement) {
+//       secondLargestElement = firstLargestElement;
+//       firstLargestElement = arr[i];
+//     } else if (
+//       arr[i] > secondLargestElement &&
+//       arr[i] !== firstLargestElement
+//     ) {
+//       secondLargestElement = arr[i];
+//     }
 
-    if (arr[i] < firstSmallestElement) {
-      secondSmallestElement = firstSmallestElement;
-      firstSmallestElement = arr[i];
-    } else if (
-      arr[i] < secondSmallestElement &&
-      arr[i] !== firstSmallestElement
-    ) {
-      secondSmallestElement = arr[i];
-    }
+//     if (arr[i] < firstSmallestElement) {
+//       secondSmallestElement = firstSmallestElement;
+//       firstSmallestElement = arr[i];
+//     } else if (
+//       arr[i] < secondSmallestElement &&
+//       arr[i] !== firstSmallestElement
+//     ) {
+//       secondSmallestElement = arr[i];
+//     }
+//   }
+
+//   return {
+//     firstLargestElement,
+//     secondLargestElement,
+//     firstSmallestElement,
+//     secondSmallestElement,
+//   };
+// }
+
+// console.log(secondLargest([1, 2, 4, 5, 6, 3, 6]));
+
+// function checkMissingNumber(array) {
+//   let missingNumber = [];
+//   for (let i = 0; i < array.length + 1; i++) {
+//     if (array[i] !== i + 1) {
+//       missingNumber.push(i + 1);
+//     }
+//   }
+
+//   return missingNumber;
+// }
+
+// console.log(checkMissingNumber([1, 2, 4, 5, 6, 7]));
+let arr = [1, 2, 3, 4, 6, 7];
+
+function insertArrayValue(arr, target, value) {
+  for (let i = arr.length; i > target; i--) {
+    arr[i] = arr[i - 1];
   }
 
-  return {
-    firstLargestElement,
-    secondLargestElement,
-    firstSmallestElement,
-    secondSmallestElement,
-  };
+  arr[target] = value;
 }
 
-console.log(secondLargest([1, 2, 4, 5, 6, 3, 6]));
+insertArrayValue(arr, 4, 5);
+
+console.log(arr);
