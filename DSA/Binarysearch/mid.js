a252df432a838c7b11d8d64cfe822cb4331d00a9
Om <23anuragsharma@gmail.com>
2025-03-10 14:14:32 +0530

    basic question solvr

---
+++ DSA/Binarysearch/mid.js
@@ -0,0 +1,8 @@
+function MissingElement(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {}
+}
+
+console.loj(MissingElement([1, 2, 3, 4, 6, 7, 8]));
---
+++ DSA/interview/question.js
@@ -0,0 +1,83 @@
+// Given an array of integers, find the smallest and largest element in the array.
+
+// Input: arr[] = {3, 5, 1, 2, 4, 8, 7}
+// Output: Smallest: 1, Largest: 8
+
+// function findMinMax(arr) {
+//   let largestValue = -Infinity;
+//   let smallestValue = Infinity;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > largestValue) {
+//       largestValue = arr[i];
+//     }
+
+//     if (arr[i] < smallestValue) {
+//       smallestValue = arr[i];
+//     }
+//   }
+
+//   return { largestValue, smallestValue };
+// }
+
+// console.log(findMinMax([7, 6, 3, 4, 5]));
+
+// Given an array of integers, write a function to reverse the array without using any extra space (in-place reversal).
+
+// let arr = [1, 2, 4, 5, 6, 7];
+
+// function reverseArray(arr) {
+//   let start = 0;
+//   let end = arr.length - 1;
+
+//   while (start < end) {
+//     let temp = arr[start];
+//     arr[start] = arr[end];
+//     arr[end] = temp;
+
+//     start++;
+//     end--;
+//   }
+
+//   return arr;
+// }
+
+// console.log(reverseArray(arr));
+
+function secondLargest(arr) {
+  let firstLargestElement = -Infinity;
+  let secondLargestElement = -Infinity;
+  let firstSmallestElement = Infinity;
+  let secondSmallestElement = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > firstLargestElement) {
+      secondLargestElement = firstLargestElement;
+      firstLargestElement = arr[i];
+    } else if (
+      arr[i] > secondLargestElement &&
+      arr[i] !== firstLargestElement
+    ) {
+      secondLargestElement = arr[i];
+    }
+
+    if (arr[i] < firstSmallestElement) {
+      secondSmallestElement = firstSmallestElement;
+      firstSmallestElement = arr[i];
+    } else if (
+      arr[i] < secondSmallestElement &&
+      arr[i] !== firstSmallestElement
+    ) {
+      secondSmallestElement = arr[i];
+    }
+  }
+
+  return {
+    firstLargestElement,
+    secondLargestElement,
+    firstSmallestElement,
+    secondSmallestElement,
+  };
+}
+
+console.log(secondLargest([1, 2, 4, 5, 6, 3, 6]));
