89ad469681391db22abd665f55697294d05ae242
Om <23anuragsharma@gmail.com>
2025-03-31 21:40:01 +0530

    Changes done

--- DSA/array/practice.js
+++ DSA/array/practice.js
@@ -18,3 +18,34 @@
 // arr[0] = lastIndexCopy;
 
 // console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5];
+
+// function leftRoatate(arr, k) {
+//   for (let j = 0; j < k; j++) {
+//     const copy = arr[0];
+//     for (let i = 0; i < arr.length - 1; i++) {
+//       arr[i] = arr[i + 1];
+//     }
+
+//     arr[arr.length - 1] = copy;
+//   }
+
+//   return arr;
+// }
+
+// console.log(leftRoatate(arr, 2));
+
+// function mergeSortedArray(arr) {
+//   let j = 1;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] != arr[i + 1]) {
+//       arr[j] = arr[i + 1];
+//       j++;
+//     }
+//   }
+// }
+
+let arr = [1, 2, 3];
+const obj = {};
+function allPermutation() {}
