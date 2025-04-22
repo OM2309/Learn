c5d5455e23deab95c4b43ba5f79c866cc5ee5eef
Om <23anuragsharma@gmail.com>
2025-04-22 23:10:39 +0530

    sliding window

--- DSA/recursion/basic.js
+++ DSA/recursion/basic.js
@@ -51,3 +51,29 @@
 // }
 
 // console.log(naturalNumbers(5));
+
+// fib nth term
+
+// let n = 10;
+// let first = 0;
+// let second = 1;
+
+// process.stdout.write(first + " " + second + " ");
+
+// for (let i = 0; i < n - 2; i++) {
+//   let third = first + second;
+//   first = second;
+//   second = third;
+//   process.stdout.write(third + " ");
+// }
+
+function fiboNTerms(n, first, second) {
+  if (n == 0) return;
+  let third = first + second;
+  process.stdout.write(third + " ");
+  fiboNTerms(n - 1, second, third);
+}
+
+let n = 10;
+process.stdout.write(0 + " " + 1 + " ");
+fiboNTerms(n - 2, 0, 1);
