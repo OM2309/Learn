3619916828c4f9a36cd1df1483c5fb09f618cd86
Om <23anuragsharma@gmail.com>
2025-06-01 23:16:41 +0530

    warm up

--- namaste-dev-dsa/loop-2/4.js
+++
@@ -1,9 +0,0 @@
-function secondLargest(arr) {
-  let sortArray = arr.sort((a, b) => b - a);
-  let firstMax = arr[0];
-  let secondMax = arr[1];
-
-  return secondMax;
-}
-
-console.log(secondLargest([1, 2, 3, 4, 5, 6, 7]));
